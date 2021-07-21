d6493f0d1bbcdcb82ab2440186d9b676e9473ade
Eti Daniel <danieleti@gmail.com>
2021-07-20 23:16:38 -0400

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray,secondArray) {
+  let sameLength = (firstArray.length === secondArray.length);
+  let equal;
+  if (sameLength) {
+    for (let i = 0; i < firstArray.length; i++) {
+      equal = (firstArray[i] === secondArray[i]);
+      if (!equal)
+        i = firstArray.length;
+    }
+  } else
+    return sameLength;
+  return equal;
+};
+
+//test
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
