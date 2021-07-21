fb9839e85c5567c31dcd036bc3880a646dcbcd0f
Eti Daniel <danieleti@gmail.com>
2021-07-21 09:13:57 -0400

    assertArrayEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(firstArray,secondArray) {
+  let sameLength = (firstArray.length === secondArray.length);
+  let equal;
+  if (sameLength) {
+    for (let i = 0; i < firstArray.length; i++) {
+      equal = (firstArray[i] === secondArray[i]);
+      if (!equal)
+        i = firstArray.length;
+    }
+  } else
+    return sameLength;
+  return equal;
+};
+
+const assertArraysEqual = function(firstArr,secondArr) {
+  assertEqual(eqArrays(firstArr,secondArr),true);
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//assertArraysEqual([],[1,1,4]);
