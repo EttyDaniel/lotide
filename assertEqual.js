873507bc1dd67f4f9a40a0b3d8ac31fe71715d97
Eti Daniel <danieleti@gmail.com>
2021-07-23 22:02:47 -0400

    adding emoji

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`👍🏼 Assertion Passed: ${actual} === ${expected}`);
   else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`👎🏼 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("1",1);
