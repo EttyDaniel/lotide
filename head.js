d3f838355f25e98a0e947484997b6337aa8957ae
Eti Daniel <danieleti@gmail.com>
2021-07-24 21:36:45 -0400

    edit head.js

--- head.js
+++ head.js
@@ -1,19 +1,20 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`👍🏼 Assertion Passed: ${actual} === ${expected}`);
   else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`👎🏼 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 
 const head = function(array) {
   let first = undefined;
-  first = array[0];
+  if (array.length > 0)
+    first = array[0];
   return first;
 };
 
 //Test
-
+assertEqual(head([]), 5);
 assertEqual(head([4]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
