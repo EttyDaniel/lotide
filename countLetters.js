9f3e640353bbb975efcd5a16bbda924d3c6d3912
Eti Daniel <danieleti@gmail.com>
2021-07-27 10:44:04 -0400

    countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`👍🏼 Assertion Passed: ${actual} === ${expected}`);
+  else {
+    console.log(`👎🏼 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let lettersCount = {};
+  for (let c in sentence) {
+    if (sentence[c] !== " ") {
+      //console.log(sentence[c]);
+      if (lettersCount[sentence[c]])
+        lettersCount[sentence[c]] += 1;
+      else
+        lettersCount[sentence[c]] = 1;
+    }
+  }
+  return lettersCount;
+};
+
+const myString = "hello Etty";
+let myStringCount = countLetters(myString);
+console.log(myStringCount);
+assertEqual(myStringCount["l"],2);
+assertEqual(myStringCount[" "],undefined);
+assertEqual(myStringCount["e"],1);
+assertEqual(myStringCount["E"],1);
